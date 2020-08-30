649cbb2ecaa4636e70e3973cacb427d43cc281e0
zeus <33592162+1nam@users.noreply.github.com>
2020-08-29 20:21:06 -0400

    Create ask_email_password.py

---
+++ ask_email_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#coding: utf-8
+x = input("Please enter your Email: ")
+password = "love"
+user = input("Password: ")
+if user == password:
+    print("Welcome your in !")
+else:
+    print("Wrong Password your entry has been sent to the website admin for review \nyou will recive an email about this event Good Bye...")
+print (x)
